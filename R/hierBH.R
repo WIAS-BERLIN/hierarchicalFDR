0633849bf8e81099ec49d5d1c6ace78e47f9f77f
Karsten Tabelow <karsten.tabelow@wias-berlin.de>
2015-03-10 10:53:08 +0100

    initial commit of the functions by Konstantin for hierarchical FDR into a new package.

---
+++ R/hierBH.R
@@ -0,0 +1,27 @@
+hierBH <-
+function(pValues,alpha=0.05){#The hierarchical testing procedure developed by Bogomolov
+result<-list()
+k<-length(pValues)
+flpvalues<-numeric()#family level p-values
+for (i in 1:k){
+	fpValues<-pValues[[i]]
+	sortfpValues<-sort(fpValues)
+	fm<-length(fpValues)
+	interpValues<-numeric()
+	for (i in 1:fm){
+		locpvalue<-(sortfpValues[[i]]*fm/i)
+		interpValues<-c(interpValues,locpvalue)
+		}
+	flpvalues<-c(flpvalues,min(interpValues))
+	}
+famlevel <- BH(flpvalues, alpha, silent=TRUE)$rejected #BH test on the family level
+for (i in 1:length(famlevel)){
+	R<-length(famlevel[famlevel[[i]]==TRUE])
+	if (famlevel[[i]]==TRUE){
+		lresult<-BH(pValues[[i]],alpha=(R/k*alpha),silent=TRUE)$rejected #BH test on the local p-values within a family
+		}
+	else {lresult<-(pValues[[i]]!=pValues[[i]])}
+	result<-c(result,list(lresult))
+	}
+return(result)
+}
---
+++ R/hierasymptkappa.R
@@ -0,0 +1,51 @@
+hierasymptkappa <-
+function(pValues, alpha=0.05 ,kappa=0.001){
+#kappa<-0.001
+result<-list()
+if (is.list(pValues)==FALSE) {#no family structure, test as one single family
+	fpValues<-sort(pValues)
+	m<-length(pValues)
+	u<-as.integer(kappa*m)+1
+	conjpValues<-numeric()
+	for (i in 1:(m-u+1)){
+		locpvalue<-(fpValues[[(u-1+i)]]*(m-u+1)/i)
+		conjpValues<-c(conjpValues,locpvalue)
+		}
+	
+	fpvalue<-min(conjpValues)#family p-value
+	if (fpvalue<=alpha*kappa)#pretest of the one family p-value
+		{hresult<-aorc(pValues,alpha,startIDX_SUD=u,betaAdjustment=0,silent=TRUE)#local result in the family by AORC test
+		result<-list(hresult)
+		}
+	else {result<-(list(fpValues!=fpValues))}#no rejections made
+	}
+
+else {
+k<-length(pValues)
+if ((1/k)<=kappa) {kappa<-(1/k)}#For larger families obtain a smaller kappa than the preset
+for (i in 1:k){
+  if (length(pValues[[i]])==0){
+    lresult<-list(FALSE);
+    result<-c(result,lresult);
+    next;
+    };
+	fpValues<-pValues[[i]]#family p-values
+	sortfpValues<-sort(fpValues)#sorted family p-values
+	fm<-length(fpValues)
+	fu<-as.integer(kappa*fm)#determine u_i
+	if (fu<(kappa*fm)){fu<-fu+1}
+	conjpValues<-numeric()
+	for (i in 1:(fm-fu+1)){
+		locpvalue<-(sortfpValues[[(fu-1+i)]]*(fm-fu+1)/i)
+		conjpValues<-c(conjpValues,locpvalue)
+		}
+	fpvalue<-min(conjpValues)#family p-value
+	if (fpvalue<=(alpha*kappa))#pretest of the family p-value
+		{lresult<-aorc(fpValues,alpha,startIDX_SUD=fu,betaAdjustment=0,silent=TRUE)#local test of the family by AORC SUD procedure
+		lresult<-list(lresult$rejected)}
+	else {lresult<-list(fpValues!=fpValues)}#no rejection in this family is made
+	result<-c(result,lresult)
+	}
+	}
+return(result)
+}
